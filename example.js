07d5d23c85360ef523cf745b4db29e05d967fdd1
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-11-22 13:35:29 +0000

    Saves to json

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 var uknii = require("./lib/main");
 
 uknii.fetchPublishers(function (err) {
-	uknii.csv2Json("./20131031_NII_DGU_I1.csv", function (err) {
+	uknii.csv2Json("./20131031_NII_DGU_I1.csv", "./20131031_NII_DGU_I1.json", function (err) {
 		console.log("Done.");
 	});
 });
