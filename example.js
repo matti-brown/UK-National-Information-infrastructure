e5f98dfbcc67fd8484ff059b05aa63af0d526dcf
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-11-22 13:39:13 +0000

    Supports command line

--- example.js
+++ example.js
@@ -1,7 +1,13 @@
-var uknii = require("./lib/main");
+var argv = require('optimist') 
+		.usage('Usage: $0 command --in <input csv file> --out <output json file>')
+		.demand([ 'in', 'out' ])
+		.alias('in', 'i')
+		.alias('out', 'o')
+		.argv,
+	uknii = require("./lib/main");
 
 uknii.fetchPublishers(function (err) {
-	uknii.csv2Json("./20131031_NII_DGU_I1.csv", "./20131031_NII_DGU_I1.json", function (err) {
+	uknii.csv2Json(argv.in, argv.out, function (err) {
 		console.log("Done.");
 	});
 });
